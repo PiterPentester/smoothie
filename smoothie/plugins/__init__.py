e606e533435ed3cb66a4dac1ab77546ffce8f76c
David Francos Cuartero <dfrancos@buguroo.com>
2016-01-10 01:16:33 +0100

    Added target handling

--- smoothie/plugins/__init__.py
+++ smoothie/plugins/__init__.py
@@ -2,4 +2,4 @@
 
 from smoothie.plugins.interfaces import Interfaces as interfaces
 from smoothie.plugins.list_networks import ListNetworks as list_networks
-
+from smoothie.plugins.target_networks import TargetNetworks as target_networks
