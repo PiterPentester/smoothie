e7e9626f314964d2f4d974831e838f299de418a8
David Francos Cuartero <dfrancos@buguroo.com>
2016-04-19 02:08:16 +0200

    Basic template

--- smoothie/__init__.py
+++ smoothie/__init__.py
@@ -1,17 +1,20 @@
 """
     Smoothie
 """
+import os
 import json
 import asyncio
+import logging
 import aiohttp
 import aiohttp.web
+import jinja2
 import aiohttp_jinja2
-import logging
 import rethinkdb as r
 
 r.set_loop_type("asyncio")
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
+CURR = os.path.dirname(__file__)
 DB_NAME = "smoothie"
 TABLE_NAME = "plugins"
 
@@ -26,7 +29,8 @@ async def websocket_handler(request):
     await ws_.prepare(request)
 
     conn = await r.connect()
-    cursor = await r.db(DB_NAME).table(TABLE_NAME).changes().run(conn)
+    cursor = await r.db(DB_NAME).table(TABLE_NAME).changes(
+        initial_changes=True).run(conn)
     while await cursor.fetch_next():
         ws_.send_str(json.dumps(await cursor.next()))
 
@@ -34,23 +38,32 @@ async def websocket_handler(request):
 
 
 class Task(aiohttp.web.View):
+    @aiohttp_jinja2.template('task.jinja2')
     async def get(self):
-        conn = await r.connect()
-        cursor = await r.db(DB_NAME).table(TABLE_NAME)
-        await self.request.GET['task_id']
-        return await
+        return {'task_id': self.request.GET['task_id']}
 
     async def post(self):
-        return await
+        """ Create a task. """
+        conn = await r.connect()
+        await r.db(DB_NAME).table(TABLE_NAME).insert(
+            await self.request.post()).run(conn)
+        return aiohttp.web.Response(body='{}')
 
+
+@aiohttp_jinja2.template('index.jinja2')
+def main_handler(request):
+    """ Main page """
+    return {}
 
 
 async def init(loop):
     """ init """
     app_ = aiohttp.web.Application(loop=loop)
-    aiohttp_jinja2.setup(app_, loader=jinja2.FileSystemLoader('./templates/'))
-    app_.router.add_route('GET', '/ws', View)
-    app_.router.add_route('GET', '/', websocket_handler)
+    aiohttp_jinja2.setup(app_, loader=jinja2.FileSystemLoader(
+        os.path.join(CURR, './templates/')))
+    app_.router.add_route('*', '/task', Task)
+    app_.router.add_route('GET', '/ws', websocket_handler)
+    app_.router.add_route('GET', '/', main_handler)
     await loop.create_server(app_.make_handler(), '127.0.0.1', 9000)
     return app_
 
